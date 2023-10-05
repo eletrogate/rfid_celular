3d979eac4dfccae6b435d8a06128ef8c0a66e415
Rafael.Faleiros <65898909+fdstfdtscdbh@users.noreply.github.com>
2023-10-05 14:15:28 -0300

    Update leituraTAG.ino

--- RFID_ELETROGATE_porRafaelFaleiros/leituraTAG.ino
+++ RFID_ELETROGATE_porRafaelFaleiros/leituraTAG.ino
@@ -14,9 +14,9 @@ void leituraTAG() {
       escreve(rfidTag, 0, 1, 0); // Exibe o conteúdo da tag RFID no LCD
       Serial.println("Tag RFID cadastrada.");
     // aciona a trava por 3seg
-      digitalWrite(trava,HIGH);
-      delay(3000);
       digitalWrite(trava,LOW);
+      delay(3000);
+      digitalWrite(trava,HIGH);
     } else {
       escreve("TAG incorreta", 0, 0, 1); // Exibe "TAG incorreta" no LCD
       escreve(rfidTag, 0, 1, 0); // Exibe o conteúdo da tag RFID no LCD
